0c10721de76d458a13dadf68700ae176ed1e4bfa
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2024-06-03 09:26:51 -0700

    p

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,26 +5,27 @@ their TODO list progress"""
 import requests
 import sys
 
+if __name__ == "__main__":
 
-complete = 0
-count = 0
-user_id = int(sys.argv[1])
-todosurl = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
-usersurl = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+    complete = 0
+    count = 0
+    user_id = int(sys.argv[1])
+    todosurl = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
+    usersurl = f'https://jsonplaceholder.typicode.com/users/{user_id}'
 
-usersresponse = requests.get(usersurl)
-users = usersresponse.json()
+    usersresponse = requests.get(usersurl)
+    users = usersresponse.json()
 
-todosresponse = requests.get(todosurl)
-todos = todosresponse.json()
-for todo in todos:
-    count += 1
-    if todo['completed'] is True:
-        complete += 1
-print("Employee {} is done with tasks({}/{})".format(
-    users['name'], complete, count))
-
-if todos is not None:
+    todosresponse = requests.get(todosurl)
+    todos = todosresponse.json()
     for todo in todos:
+        count += 1
         if todo['completed'] is True:
-            print(f"\t {todo['title']}")
+            complete += 1
+    print("Employee {} is done with tasks({}/{})".format(
+        users['name'], complete, count))
+
+    if todos is not None:
+        for todo in todos:
+            if todo['completed'] is True:
+                print(f"\t {todo['title']}")
