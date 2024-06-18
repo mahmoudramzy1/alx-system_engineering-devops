5b08c7db8f34d637fe6f98b4b9678dcb14105c78
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-06-18 16:02:21 -0700

    p

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,15 +3,15 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ function to get subscriber count"""
     subscribers = 0
     base_url = "https://www.reddit.com/r/"
     url = "{}{}/about.json".format(base_url, subreddit)
-    res = requests.get(url, 
-              headers = {"User-Agent": "My-User-Agent"},
-              allow_redirects=False
-              )
+    res = requests.get(url,
+                       headers={"User-Agent": "My-User-Agent"},
+                       allow_redirects=False)
     if res.status_code == 200:
         data = res.json()
         subscribers = data.get('data', {}).get('subscribers', 0)
